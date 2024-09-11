0d531f5cbd2bc4ecfaeda9630912fe42dbf4c7c1
tobydjones <tobydjones@gmail.com>
2024-09-11 23:29:52 +0200

    Add files via upload

--- tests/test.gd
+++ tests/test.gd
@@ -8,15 +8,15 @@ func foo():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
+	
 	$Beauty.source_lines = """
 	var x=0
 	var y:="foo"
 	var z :  =  "bar"
 	var t:int=0
-
-func test(a, b,c, d ,e, f,g):
-
+	
+func test(a, b,c, d ,e, f,g):	
+	
 	a = a        + b
 	a = a +b
 	a = a+ b
@@ -29,22 +29,22 @@ func test(a, b,c, d ,e, f,g):
 	a =- b
 	a=-b
 	a=a**2, a=a **2, a=a** 2
-
+	
 	a =! b
 	a=a&!b
-
+	
 	a=a%b % c %d  %e % f
 	var a=%Node.property
 	var a=[%Node1,‰Node2]
 	call_some_func(%Node1,%Node2)
 	x=y%%Toto.z
 	x = y % %Toto.z
-
+	
 	string=&"string"
 
 	a==b, a ==b, a== b , a[i]==b
 	a>=b, a
-	a>=b, a >=b, a>= b
+	a>=b, a >=b, a>= b 
 	a<=b, a <=b, a<= b
 	a!=b, a !=b, a!= b
 	a+=b, a +=b, a+= b
@@ -71,23 +71,22 @@ func test(a, b,c, d ,e, f,g):
 	a= b&&c &&d&& e
 	a= b||c ||d|| e
 
-func test(a:int,b:int, c, d, e, f, g)->z:
-func test(a:int, b    :   int, c, d, e, f, g) -> z :
+func test(a:int,b:int, c, d, e, f, g)->z: 
+func test(a:int, b    :   int, c, d, e, f, g) -> z : 
 	a = a  + b  + c  + d  + e
 	a = a*b
 	a=a* b *c *  d
-
-
-
-if $node/test:
+	
+	
+	
+	if $node/test:
 	var node_test = $node/test
 	var node_text = $node/test.text
 	$node/test.text="node/test"
 	$node/test = "spaces"
 	$node/test	=	"tabs"
-
-
-
+	
+	
 	""".split("\n")
 
 
@@ -96,3 +95,4 @@ if $node/test:
 	$Beauty._clean_empty_lines()
 	$Beauty._clean_end_of_script()
 	$Beauty._clean_end_of_lines()
+
